5c119d074ea48412c5b52dc7bda79ed96917a527
Stephen Colfer <stephen.colfer@ucdconnect.ie>
2021-04-22 19:40:50 +0100

    multiple color support

--- ledStripDaemon.py
+++ ledStripDaemon.py
@@ -2,6 +2,9 @@ from ledStrip import *
 from config import *
 from animations import *
 
+with open('accentColors.json', 'w') as outfile:
+    colors = json.load(json_file)
+
 def getStartEnd(note):
     drum_name = MIDI_NOTE_INDEX[note]
     start = LED_DRUM_INDEX[drum_name][0]
@@ -9,7 +12,6 @@ def getStartEnd(note):
     return start, end
 
 def ledStripDaemon(queue):
-    color = MAIN_COLOUR
     l = LedStrip(LED_COUNT)
     led_values = {}
 
@@ -19,10 +21,23 @@ def ledStripDaemon(queue):
     while True:
         try:
             j = queue.get_nowait()
-            start, end = getStartEnd(j)
+            if j["type"] == "accent":
+                start,end = 0, LED_COUNT
+                hit_color = colors["accent_hit_1_color"]
+            else:
+                start, end = getStartEnd(j["note"])
+
+            # Set Color
+            if j["type"] == "kick":
+                hit_color = colors["kick_color"]
+            elif j["type"] == "pad":
+                hit_color = colors["pad_color"]
+            elif j["type"] == "cymbal":
+                hit_color = colors["cymbal_color"]
+
+
             for k in range(end-start):
-                #led_values[k+start] = [int(COLOURS[color]['g']),int(COLOURS[color]['r']),int(COLOURS[color]['b'])]
-                led_values[k + start] = [0,0,255]
+                led_values[k + start] = hit_color
 
             for i in range(len(led_values)):
                 r = led_values[i][0]
--- main.py
+++ main.py
@@ -17,9 +17,21 @@ except:
     print("No MIDI device found. Please make sure a device is connected.")
     exit(1)
 
-# Start reading MIDI messages
-for msg in midi_connection:
-    if msg.type == "note_on":
-        if msg.note in ACTIVE_MIDI_PADS:
-            print(msg)
-            queue.put(msg.note)
+
+def listen_to_midi_notes():
+    for msg in midi_connection:
+        if msg.type == "note_on":
+            if msg.note in PADS_NOTES:
+                print(msg)
+                queue.put({"note":msg.note, "type":"pad"})
+
+            elif msg.note in CYMBAL_NOTES:
+                queue.put({"note":msg.note, "type":"cymbal"})
+
+            elif msg.note == KICK_NOTE:
+                queue.put({"note":msg.note, "type":"kick"})
+
+
+if name == "__main__":
+    # Start reading MIDI messages
+    listen_to_midi_notes()
